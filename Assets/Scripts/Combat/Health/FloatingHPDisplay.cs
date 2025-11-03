61b8b875d7cb554ee39917ba970808b352e5d8ff
Anson Leung <gaminggamer212@gmail.com>
2025-11-03 02:58:17 -0500

    wa

--- Assets/Scripts/Combat/Health/FloatingHPDisplay.cs
+++ Assets/Scripts/Combat/Health/FloatingHPDisplay.cs
@@ -4,18 +4,30 @@ using UnityEngine.UI;
 public class FloatingHPDisplay : MonoBehaviour
 {
     [SerializeField] private Slider healthBar;
+    private Image fillImage;
+    public Color maxColor = Color.green;
+    public Color minColor = Color.red;
     private Camera cam;
     public Transform target;
     public Vector3 offset;
+    
+    private float ratio;
+    private float adjustedRatio;
 
     void Start()
     {
         cam = Camera.main;
+        fillImage = healthBar.fillRect.GetComponent<Image>();
+        fillImage.color = maxColor;
     }
     
     public void UpdateHealthBar(float currHealth, float maxHealth)
     {
-        healthBar.value = Mathf.Clamp01(currHealth / maxHealth);
+        ratio = Mathf.Clamp01(currHealth / maxHealth);
+        healthBar.value = ratio;
+        
+        adjustedRatio = Mathf.Pow(ratio, 1.3f);
+        fillImage.color = Color.Lerp(minColor, maxColor, adjustedRatio);
     }
 
     void Update()
