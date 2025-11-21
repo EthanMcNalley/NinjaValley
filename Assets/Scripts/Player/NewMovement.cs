9d58df2024b211d8535338b9cb7e68554ecfaf78
EthanMcNalley <ethanmcnalley13@gmail.com>
2025-11-21 12:34:45 -0500

    sigh

--- Assets/Scripts/Player/NewMovement.cs
+++ Assets/Scripts/Player/NewMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine.InputSystem;
 public class NewMovement : MonoBehaviour
 { 
     public float playerSpeed = 5.0f;
-    public float jumpHeight = 1.5f;
     public float gravityValue = -9.81f;
 
     private CharacterController controller;
