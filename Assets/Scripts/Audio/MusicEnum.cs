c3ead16afb9c66b87890b239e750f385e0f68b2e
Anson Leung <gaminggamer212@gmail.com>
2025-11-24 22:50:19 -0500

    pus

--- Assets/Scripts/Audio/MusicEnum.cs
+++ Assets/Scripts/Audio/MusicEnum.cs
@@ -1,10 +1,7 @@
 public enum MusicEnum
 {
-    Area_1 = 0,
-    Bingus_Boss = 1,
-    Ambiance = 2,
-    Area_2 = 3,
-    Final_Phase1 = 4,
-    Silence = 5,
-    Final_Phase2 = 6
+    Tutorial = 0,
+    WaterDrip = 1,
+    Area1 = 2,
+    Silence = 3
 }
