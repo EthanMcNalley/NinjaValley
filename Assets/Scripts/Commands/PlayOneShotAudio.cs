65b200a05f1424c66f37b11d4c67c7d2e86770f8
Anson Leung <gaminggamer212@gmail.com>
2026-02-17 05:11:28 -0500

    waw

--- Assets/Scripts/Commands/PlayOneShotAudio.cs
+++ Assets/Scripts/Commands/PlayOneShotAudio.cs
@@ -5,6 +5,11 @@ public class PlayOneShotAudio : MonoBehaviour
 {
     public EventReference sound_effect;    
     public void PlayTheOneShot(){
+        if (sound_effect.IsNull)
+        {
+            return;
+        }
+        
         AudioManager.instance.PlayOneShot(sound_effect, transform.position);
     }
 }
