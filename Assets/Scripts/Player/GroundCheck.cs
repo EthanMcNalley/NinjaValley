351c69f9740105b6153322530753b2afe8ef2ff1
EthanMcNalley <ethanmcnalley13@gmail.com>
2026-02-23 09:16:47 -0500

    hmm

--- Assets/Scripts/Player/GroundCheck.cs
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -6,19 +6,45 @@ public class GroundCheck : MonoBehaviour
     public Transform groundCheck;
     public float GroundCheckRadius;
     public LayerMask[] groundLayer;
+    public LayerMask child_ground_layer;
     [SerializeField] private bool isGrounded;
+    public GameObject parent_reset;
     
     private void Update()
     {
+        Collider[] child_ground = Physics.OverlapSphere(groundCheck.position, GroundCheckRadius, child_ground_layer, QueryTriggerInteraction.Ignore);
+        if (child_ground.Length > 0)
+        {
+            transform.parent = child_ground[0].gameObject.transform.parent;
+            // for (int i = 0; i < child_ground.Length; i++)
+            // {
+            //     if (child_ground[i])
+            // }
+        }
+
+        else
+        {
+            transform.parent = parent_reset.transform;
+            transform.parent = null;
+        }
+
         for (int i = 0; i < groundLayer.Length; i++)
         {
-            isGrounded = Physics.CheckSphere(groundCheck.position, GroundCheckRadius, (int)groundLayer[i]);
+            Collider[] grounds = Physics.OverlapSphere(groundCheck.position, GroundCheckRadius, groundLayer[i], QueryTriggerInteraction.Ignore);
+            isGrounded = grounds.Length > 0;
             if (isGrounded)
             {
                 break;
             }   
         }
+
+
     }
 
+     void OnDrawGizmos()
+    {
+        Gizmos.color = Color.darkBlue;
+        Gizmos.DrawWireSphere(groundCheck.position, GroundCheckRadius);
+    }
     public bool IsGrounded => isGrounded;
 }
